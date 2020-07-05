3d6107c0f8c77a3880371eacb7699cba18dbd028
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-05 20:37:53 +0600

    Fixed modile name in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from .routers import generate
 
 def create_app():
     app = FastAPI(docs_url='/api/docs',
                   openapi_url="/api/openapi.json")
 
     app.include_router(
-        motion.router,
-        prefix="/api/motion",
+        generate.router,
+        prefix="/api/generate",
     )
 
     return app
