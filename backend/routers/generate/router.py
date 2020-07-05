1cf3e995c491cc51b672e0d84d08547ec5e9ac9f
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-05 21:33:13 +0600

    Fixed bugs

--- backend/routers/generate/router.py
+++ backend/routers/generate/router.py
@@ -6,16 +6,17 @@ from .models import GenerateRequest, GenerateResponse
 
 router = APIRouter()
 
-model = LanguageGenerationModel("gpt2", "./artifacts/model",
+model = LanguageGenerationModel("gpt2", "./routers/generate/artifacts/model",
         args={"length": 256, 'fp16': False}, use_cuda=False)
 
-@router.post("/", response_model=MotionResponse)
-async def generate_motion(request: MotionRequest):
+@router.post("/", response_model=GenerateResponse)
+async def generate_motion(request: GenerateRequest):
     prefix = request.prefix
     temperature = request.temperature
 
-    model_input = [prefix] * 5
+    results = []
 
-    results = model.generate(model_input, verbose=False, args={'temperature':0.9})
+    for _ in range(5):
+        results += model.generate(prefix, verbose=False, args={'temperature':temperature})
 
-    return MotionResponse(motions=results)
+    return GenerateResponse(motions=results)
