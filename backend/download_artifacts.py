b1247ef00605175902681415d2c98c412d191256
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-23 01:05:56 +0600

    Added downloading script

---
+++ backend/download_artifacts.py
@@ -0,0 +1,38 @@
+import os
+import json
+import zipfile
+import logging
+from google.cloud import storage
+from google.oauth2 import service_account
+
+logging.basicConfig(level=logging.INFO)
+
+project_id = 'motiongenerator'
+bucket_name = 'motiongenerator'
+model_zip = 'atomic-dawn-43.zip'
+
+service_account_info = json.loads(
+        os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+)
+
+credentials = service_account.Credentials.from_service_account_info(
+        service_account_info
+)
+
+client = storage.Client(project=project_id, credentials=credentials)
+
+if __name__ == '__main__':
+    bucket = client.get_bucket(bucket_name)
+    logging.info("Was successfully authorized.")
+
+    blob = bucket.get_blob(model_zip)
+    logging.info("Started downloading file")
+    blob.download_to_filename(os.path.join('artifacts', model_zip))
+    logging.info("Finished downloading file")
+
+    with zipfile.ZipFile(os.path.join('artifacts', model_zip), "r") as zip_ref:
+        logging.info("Unzipping archive")
+        zip_ref.extractall(os.path.join('artifacts', 'model'))
+
+    os.remove(os.path.join('artifacts', model_zip))
+    logging.info("Deleted archive")
