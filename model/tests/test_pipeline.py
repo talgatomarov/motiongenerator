48da96528bb6f499d3b0a2055a22f96698017fb7
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-22 14:34:26 +0600

    Made flint8 happy

--- model/tests/test_pipeline.py
+++ model/tests/test_pipeline.py
@@ -30,28 +30,35 @@ def test_env_vars():
     assert os.environ['WANDB_WATCH'] == 'all'
     assert 'GOOGLE_APPLICATION_CREDENTIALS_JSON' in os.environ
 
+
 @pytest.fixture(scope='session', autouse=True)
 def cleanup():
     yield
-    if os.path.exists(test_data_folder): shutil.rmtree(test_data_folder)
+    if os.path.exists(test_data_folder):
+        shutil.rmtree(test_data_folder)
+
 
 def test_run():
-    data_folder = luigi.configuration.get_config().set('GlobalConfig', 'data_folder', test_data_folder)
+    luigi.configuration.get_config().set('GlobalConfig', 'data_folder', test_data_folder)
     result = luigi.build([DownloadDataset(bucket=test_bucket, filename=test_filename),
                           PreprocessDataset(eos_token=eos_token, bos_token=bos_token),
                           SplitDataset(),
                           Train(num_train_epochs=1, block_size=64)],
-                          local_scheduler=True, detailed_summary=True)
+                         local_scheduler=True,
+                         detailed_summary=True)
 
     assert result.status == LuigiStatusCode.SUCCESS or \
            result.status == LuigiStatusCode.SUCCESS_WITH_RETRY
 
+
 def test_download():
-    assert os.path.isfile(os.path.join(test_data_folder, motions_file)) # dataset was not downloaded
+    assert os.path.isfile(os.path.join(test_data_folder, motions_file))  # dataset was not downloaded
+
 
 def test_preprocess():
-    assert os.path.isfile(os.path.join(test_data_folder, motions_prep_file)) # dataset was not preprocessed
+    assert os.path.isfile(os.path.join(test_data_folder, motions_prep_file))  # dataset was not preprocessed
+
 
 def test_split():
-    assert os.path.isfile(os.path.join(test_data_folder, train_file)) # train file was not created
-    assert os.path.isfile(os.path.join(test_data_folder, test_file)) # test file was not created
+    assert os.path.isfile(os.path.join(test_data_folder, train_file))  # train file was not created
+    assert os.path.isfile(os.path.join(test_data_folder, test_file))  # test file was not created
