24fb0bb9c81a3899f55a30bf197eb8c281bd39fc
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-22 23:35:19 +0600

    Changed backend prediction model

--- backend/routers/generate/router.py
+++ backend/routers/generate/router.py
@@ -1,26 +1,31 @@
 from fastapi import APIRouter
-from simpletransformers.language_generation import LanguageGenerationModel
-
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 from .models import GenerateRequest, GenerateResponse
 
 router = APIRouter()
 
-model = LanguageGenerationModel("gpt2", "artifacts/model",
-        args={"length": 256, 'fp16': False}, use_cuda=False)
+model = GPT2LMHeadModel.from_pretrained("artifacts/model")
+tokenizer = GPT2Tokenizer.from_pretrained("artifacts/model")
+stop_token = '<|endoftext|>'
+
 
 @router.post("/", response_model=GenerateResponse)
 async def generate_motion(request: GenerateRequest):
     prefix = request.prefix
     temperature = request.temperature
 
-    results = model.generate(prefix, verbose=False,
-            args={
-                'temperature':temperature,
-                'repetition_penalty': 2,
-                'num_return_sequences': 5,
-                'top_p': 0.95,
-                'stop_token': '.'
-                })
+    inputs = tokenizer.encode(prefix, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=64, do_sample=True,
+                             num_beams=1, top_p=0.99,
+                             early_stopping=True, num_return_sequences=5,
+                             temperature=temperature, repetition_penalty=5)
+
+    results = []
+
+    for output in outputs:
+        text = tokenizer.decode(output)
+        text = text[: text.find(stop_token) if stop_token else None]
+        results.append(text)
 
     return GenerateResponse(motions=results)
