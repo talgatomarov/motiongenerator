fc00343bb31ddb2e4a59f7e475f1782a7911e60f
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-28 19:58:34 +0600

    Implemented Helper component

---
+++ frontend/src/components/home/Helper.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(3)
+  },
+  title: {
+    paddingBottom: theme.spacing(2)
+  },
+  text: {
+    lineHeight: "32px",
+    fontWeight: 300
+  }
+}));
+
+const Helper = (props) => {
+  const classes = useStyles();
+
+  return render ( 
+    <Paper className={classes.paper}>
+      <Typography variant="h6" className={classes.title}>
+        {props.title}
+      </Typography>
+
+      <Typography variant="body2" className={classes.text}>
+        {props.children}
+      </Typography>
+    </Paper>
+  );
+};
+
+export default Helper
