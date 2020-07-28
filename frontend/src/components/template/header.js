12506e38f22b186f28c3bcf6b756dbc7a81611a8
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-28 22:33:15 +0600

    Increased padding of header

--- frontend/src/components/template/header.js
+++ frontend/src/components/template/header.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   header: {
-    paddingBottom: theme.spacing(4)
+    paddingBottom: theme.spacing(8)
   },
 }));
 
