90af81bb58b32c3bdf68e39063512f62858db996
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-28 20:52:43 +0600

    Added helper top padding

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Template from '../template'
 import Hero from './hero'
 import Grid from '@material-ui/core/Grid';
 import MotionForm from './motion-form'
 import Helper from './Helper'
 
+const useStyles = makeStyles((theme) => ({
+  helperContainer: {
+    paddingTop: theme.spacing(4)
+  }
+}));
+
 const Home = (props) => {
+  const classes = useStyles();
+
   return (
     <Template>
       <Hero/>
 
       <MotionForm/>
 
-      <Grid container spacing={6}>
+      <Grid container spacing={6} className={classes.helperContainer}>
         <Grid item lg={6} md={6} xs={12}>
           <Helper title="What is a debating motion?">
             A motion, also known as a proposition or resolution, is a statement that usually sets the topic for the given debate. It usually looks like: <i>"This house would ban gun ownership"</i> or <i>"This house believes that charities should not accept money from donors they consider immoral"</i>.
