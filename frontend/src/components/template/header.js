16e1d755dd76a8c94cddb5d838cb83e1e1932e0c
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-28 18:40:53 +0600

    Added footer

--- frontend/src/components/template/header.js
+++ frontend/src/components/template/header.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    paddingBottom: theme.spacing(4)
+  },
+}));
 
 const Header = (props) => {
+  const classes = useStyles();
+
   return (
-    <div>
-    </div>
+    <Grid container className={classes.header}>
+      <Grid item lg={4} md={4} xs={12}>
+        <Typography variant="h6">motiongenerator</Typography>
+      </Grid>
+    </Grid>
   );
 };
 
