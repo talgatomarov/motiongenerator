41229a86dee752531ce99c60fd2b75154883a32f
talgatomarov <talgat.omarov.1999@gmail.com>
2020-07-28 20:03:11 +0600

    Added Helper to home page

--- frontend/src/components/home/Helper.js
+++ frontend/src/components/home/Helper.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -18,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
 const Helper = (props) => {
   const classes = useStyles();
 
-  return render ( 
+  return ( 
     <Paper className={classes.paper}>
       <Typography variant="h6" className={classes.title}>
         {props.title}
--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import Template from '../template'
 import Hero from './hero'
+import Grid from '@material-ui/core/Grid';
 import MotionForm from './motion-form'
+import Helper from './Helper'
 
 const Home = (props) => {
   return (
     <Template>
       <Hero/>
+
       <MotionForm/>
+
+      <Grid container spacing={6}>
+        <Grid item lg={6} md={6} xs={12}>
+          <Helper title="What is a debating motion?">
+            A motion, also known as a proposition or resolution, is a statement that usually sets the topic for the given debate. It usually looks like: <i>"This house would ban gun ownership"</i> or <i>"This house believes that charities should not accept money from donors they consider immoral"</i>.
+          </Helper>
+        </Grid>
+
+        <Grid item lg={6} md={6} xs={12}>
+          <Helper title="What is a natural language model?">
+            A statistical language model is a probability distribution over sequences of words. It can be used to predict the next most probable word(s). The model used in this web-application is based upon distillGPT2 model.
+          </Helper>
+        </Grid>
+      </Grid>
     </Template>
+
   );
 };
 
