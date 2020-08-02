38c42bb339d7a95505cce7d8d655c253463709ac
talgatomarov <talgat.omarov.1999@gmail.com>
2020-08-02 14:11:06 +0600

    Exposed Firebase config

--- frontend/src/base.js
+++ frontend/src/base.js
@@ -2,10 +2,18 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 
 
-const firebaseConfig = JSON.parse(process.env['FIREBASE_CONFIG']);
-const app = firebase.initializeApp(firebaseConfig);
+const firebaseConfig={
+  apiKey: "AIzaSyCQ43yRZaNL1qi9RFEDavrken9r53xtRFU",
+  authDomain: "motiongenerator.firebaseapp.com",
+  databaseURL: "https://motiongenerator.firebaseio.com",
+  projectId: "motiongenerator",
+  storageBucket: "motiongenerator.appspot.com",
+  messagingSenderId: "685271605729",
+  appId: "1:685271605729:web:8d119bfdb93eacbadaf2a0",
+  measurementId: "G-P6ZDH1HQ2V"
+}
 
-console.log(firebaseConfig.projectId);
+const app = firebase.initializeApp(firebaseConfig);
 
 export const analytics = firebase.analytics();
 export default app;
