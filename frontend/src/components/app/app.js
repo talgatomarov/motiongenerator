d62db62a53e9a193a496082ec6b6514ff968f65a
talgatomarov <talgat.omarov.1999@gmail.com>
2020-08-02 23:59:57 +0600

    Renamed app.css to App.css

--- frontend/src/components/app/app.js
+++ frontend/src/components/app/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "../home";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import "./app.css";
+import "./App.css";
 
 const theme = createMuiTheme({
   typography: {
