349efd379ae6f29cb61234c6755fb14a7982197e
talgatomarov <talgat.omarov.1999@gmail.com>
2020-08-03 09:37:39 +0600

    Added test descriptions

--- frontend/src/components/App/index.test.js
+++ frontend/src/components/App/index.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./index";
 
-test("Renders withour crashing", () => {
-  render(<App />);
+describe("App", () => {
+  test("Renders withour crashing", () => {
+    render(<App />);
+  });
 });
--- frontend/src/components/Home/MotionForm/index.test.js
+++ frontend/src/components/Home/MotionForm/index.test.js
@@ -20,37 +20,41 @@ const mockMotions = {
   },
 };
 
-test("Render MotionForm", () => {
-  render(<MotionForm />);
-});
-
-test("Test successful API call", async () => {
-  axios.post.mockImplementationOnce(() => Promise.resolve(mockMotions));
-
-  render(<MotionForm />);
-
-  const generateButton = screen.getByTestId("generate-button");
-  const generateTextField = screen.getByTestId("generate-text-field");
-
-  fireEvent.change(generateTextField, { target: { value: "This house" } });
-  expect(generateTextField).toHaveValue("This house");
-
-  fireEvent.click(generateButton);
-  expect(axios.post).toHaveBeenCalledWith("/api/generate", {
-    prefix: "This house",
-    temperature: 0.7,
+describe("MotionForm", () => {
+  test("Render MotionForm", () => {
+    render(<MotionForm />);
   });
 
-  expect(screen.getByTestId("motion-modal")).toBeInTheDocument();
-  expect(screen.queryByTestId("motion-list")).toBeNull();
-  expect(screen.getByTestId("loading-message")).toBeInTheDocument();
+  test("Test successful API call", async () => {
+    axios.post.mockImplementationOnce(() => Promise.resolve(mockMotions));
 
-  await waitForElementToBeRemoved(() => screen.getByTestId("loading-message"));
+    render(<MotionForm />);
 
-  expect(screen.queryByTestId("loading-message")).toBeNull();
-  expect(screen.getByTestId("motion-list")).toBeInTheDocument();
+    const generateButton = screen.getByTestId("generate-button");
+    const generateTextField = screen.getByTestId("generate-text-field");
 
-  mockMotions.data.motions.forEach((motion) =>
-    expect(screen.getByText(motion)).toBeInTheDocument()
-  );
+    fireEvent.change(generateTextField, { target: { value: "This house" } });
+    expect(generateTextField).toHaveValue("This house");
+
+    fireEvent.click(generateButton);
+    expect(axios.post).toHaveBeenCalledWith("/api/generate", {
+      prefix: "This house",
+      temperature: 0.7,
+    });
+
+    expect(screen.getByTestId("motion-modal")).toBeInTheDocument();
+    expect(screen.queryByTestId("motion-list")).toBeNull();
+    expect(screen.getByTestId("loading-message")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() =>
+      screen.getByTestId("loading-message")
+    );
+
+    expect(screen.queryByTestId("loading-message")).toBeNull();
+    expect(screen.getByTestId("motion-list")).toBeInTheDocument();
+
+    mockMotions.data.motions.forEach((motion) =>
+      expect(screen.getByText(motion)).toBeInTheDocument()
+    );
+  });
 });
